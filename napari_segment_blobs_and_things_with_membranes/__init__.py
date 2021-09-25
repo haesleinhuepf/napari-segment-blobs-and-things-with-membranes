5d660b0be75cb130cb4afe15182a29e7790d90b2
Robert Haase <haesleinhuepf@users.noreply.github.com>
2021-09-25 17:23:48 +0200

    bugfix: subtract_background was missing in public API

--- napari_segment_blobs_and_things_with_membranes/__init__.py
+++ napari_segment_blobs_and_things_with_membranes/__init__.py
@@ -5,5 +5,5 @@ __version__ = "0.1.3"
 
 
 
-from ._function import napari_experimental_provide_function, gaussian_blur, threshold_otsu, binary_invert, binary_watershed, connected_components_labeling, seeded_watershed, voronoi_otsu_labeling
+from ._function import napari_experimental_provide_function, gaussian_blur, subtract_background, threshold_otsu, binary_invert, binary_watershed, connected_components_labeling, seeded_watershed, voronoi_otsu_labeling
 
